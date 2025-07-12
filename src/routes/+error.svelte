086064e40d249306778aa4d72ca6f07ccc34c248
B1ANK3 <44206247+B1ANK3@users.noreply.github.com>
2025-07-12 21:03:50 +0200

    Migrated to svelte 5

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+    import { page } from '$app/stores'
 </script>
 
 <svelte:head>
-	<title>Error</title>
-	<meta name="description" content="An error occurred with the site" />
+    <title>Error</title>
+    <meta name="description" content="An error occurred with the site" />
 </svelte:head>
 
 <div class="container">
-	<div class="status">{$page.status}</div>
+    <div class="status">{$page.status}</div>
 </div>
 
 <style>
-	.container {
-		width: 100vw;
-		height: 100vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
+    .container {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-		background-color: #282828;
-	}
+        background-color: #282828;
+    }
 
-	.status {
-		font-family: 'Roboto Mono', monospace;
-		font-style: normal;
-		font-size: 12em;
-		align-content: center;
-		color: #ea6962;
-	}
+    .status {
+        font-family: 'Roboto Mono', monospace;
+        font-style: normal;
+        font-size: 12em;
+        align-content: center;
+        color: #ea6962;
+    }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+    import { page } from '$app/state'
 </script>
 
 <svelte:head>
-	<title>B1ANK3</title>
-	<meta name="description" content="A 200 response page for my github pages" />
+    <title>B1ANK3</title>
+    <meta name="description" content="A 200 response page for my github pages" />
 </svelte:head>
 
 <div class="container">
-	<div class="status">{$page.status}</div>
+    <div class="status">{page.status}</div>
 </div>
 
 <style>
-	.container {
-		width: 100vw;
-		height: 100vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
+    .container {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-		background-color: #282828;
-	}
+        background-color: #282828;
+    }
 
-	.status {
-		font-family: 'Roboto Mono', monospace;
-		font-style: normal;
-		font-size: 12em;
-		align-content: center;
-		color: #a9b665;
-	}
+    .status {
+        font-family: 'Roboto Mono', monospace;
+        font-style: normal;
+        font-size: 12em;
+        align-content: center;
+        color: #a9b665;
+    }
 </style>
