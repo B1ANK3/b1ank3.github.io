8abaa332e1137d6d3226a83576684367ee4ec37d
B1ANK3 <44206247+B1ANK3@users.noreply.github.com>
2025-07-12 21:03:50 +0200

    Migrated to svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,18 @@
 <script>
-	import './style.css';
-	import RobotoMono from '$lib/fonts/Roboto-mono.woff2';
+    import './style.css'
+    import RobotoMono from '$lib/fonts/Roboto-mono.woff2'
 </script>
 
 <svelte:head>
-	<meta name="robots" content="noindex" />
-	<link rel="preload" as="font" href={RobotoMono} type="font/woff2" crossorigin="" />
+    <meta name="robots" content="noindex" />
+    <link rel="preload" as="font" href={RobotoMono} type="font/woff2" crossorigin="" />
 </svelte:head>
 
 <slot />
 
 <style>
-	@font-face {
-		font-family: 'Roboto Mono';
-		src: url('$lib/fonts/Roboto-mono.woff2') format('woff2');
-	}
+    @font-face {
+        font-family: 'Roboto Mono';
+        src: url('$lib/fonts/Roboto-mono.woff2') format('woff2');
+    }
 </style>
