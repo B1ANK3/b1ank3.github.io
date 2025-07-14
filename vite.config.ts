12319f70ac0bfaff3e14fa5c93cada53b46c939b
B1ANK3 <44206247+B1ANK3@users.noreply.github.com>
2025-07-15 00:51:28 +0200

    [WIP] Updated to svelte 5. Added UnoCSS, GunJS.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
+import UnoCSS from '@unocss/svelte-scoped/vite'
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [
+        UnoCSS({
+            onlyGlobal: true,
+            injectReset: '@unocss/reset/sanitize/sanitize.css'
+        }),
+        sveltekit()
+    ],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
     }
